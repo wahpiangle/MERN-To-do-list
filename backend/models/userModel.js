7a60cf012729f44d8d8c039e6de229cdaaa65638
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-04-25 19:00:32 +0800

    signup feature (hashing)

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -10,8 +11,24 @@ const userSchema = new Schema({
     },
     password:{
         type: String,
-        required: true
+        required: true,
     },
 })
+
+//static signup method
+userSchema.statics.signup = async function (email, password) {
+    const exists = await this.findOne({ email });
+
+    if(exists){
+        throw Error('Email already exists.');
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password : hash })
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
