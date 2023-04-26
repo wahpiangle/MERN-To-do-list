87d1ae109d1e4261368ffd486d565dc6d9026249
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-04-26 11:57:14 +0800

    password email validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,7 +15,7 @@ const signupUser = async(req, res) => {
         res.status(200).json({user})
 
     } catch(error){
-        res.status(400).json({error: error.msg})
+        res.status(400).json({error: error.message})
     }
 }
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator')
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,18 @@ const userSchema = new Schema({
 //static signup method
 userSchema.statics.signup = async function (email, password) {
     const exists = await this.findOne({ email });
+
+    if(!email || !password){
+        throw Error('All fields must be filled');
+    }
+
+    if(!validator.isEmail(email)){
+        throw Error('Email is invalid');
+    }
+
+    if(!validator.isStrongPassword(password)){
+        throw Error('Password is not strong enough');
+    }
 
     if(exists){
         throw Error('Email already exists.');
