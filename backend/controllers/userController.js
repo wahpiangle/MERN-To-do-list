c440e190be15ec6bff7c1d7727d72a7e9fd6d187
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-04-27 14:52:21 +0800

    jwt feature

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+//create web token function
+const createToken = (_id) =>{
+    return jwt.sign({_id: _id}, process.env.SECRET_KEY, {expiresIn: '1d'})
+}
 
 //login user
 const loginUser = async(req, res) => {
@@ -12,7 +18,9 @@ const signupUser = async(req, res) => {
     try{
         const user = await User.signup(email, password)
 
-        res.status(200).json({user})
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
 
     } catch(error){
         res.status(400).json({error: error.message})
