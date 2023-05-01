343fa8c82fbcbb63b6dae79d4a5e49d6e3643a7d
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-05-01 18:36:19 +0800

    logout feature

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom"
+import { useLogout } from "../hooks/useLogout"
 
 export default function NavBar() {
+
+    const { logout } = useLogout()
+
+    const handleLogout = () =>{
+        logout()
+    }
+
     return(
         <header className="bg-[#fff] flex justify-between">
             <div className="my-auto px-8 py-3 flex items-center font-bold text-[24px]">
@@ -8,7 +16,10 @@ export default function NavBar() {
                     <h1>Workout Buddy</h1>
                 </Link>
             </div>
-            <nav className="flex items-center mr-4">
+            <nav className="flex items-center mr-4 gap-4">
+                <div>
+                    <button className="text-green-500 border-2 border-green-500 bg-white p-2 font-semibold" onClick={handleLogout}>Logout</button>
+                </div>
                 <div className="font-semibold">
                     <Link to="/login" className="mr-4">Login</Link>
                 </div>
