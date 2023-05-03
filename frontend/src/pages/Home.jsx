9fdfe2b73e586bf350d6497a07996f134a5a4a9e
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-05-03 21:02:06 +0800

    update to upload on render

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,8 +11,7 @@ export default function Home() {
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            // const response = await fetch('https://workout-buddy-api-smgt.onrender.com/api/workouts');
-            const response = await fetch('http://localhost:4000/api/workouts',
+            const response = await fetch('https://workout-buddy-api-smgt.onrender.com/api/workouts',
                 {
                     headers: {
                         'Authorization': `Bearer ${user.token}`
